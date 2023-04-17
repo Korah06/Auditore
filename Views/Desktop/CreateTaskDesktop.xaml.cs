c2bbfa410ffaa970be7162865907c08a306a438e
Mario Espasa <mario.espasa.2003@gmail.com>
2023-04-17 10:39:16 +0200

    datepicker basic functionality done

--- Views/Desktop/CreateTaskDesktop.xaml.cs
+++ Views/Desktop/CreateTaskDesktop.xaml.cs
@@ -1,5 +1,6 @@
 using Auditore.Services.Interfaces;
 using Auditore.ViewModels;
+using System.Diagnostics;
 
 namespace Auditore.Views.Desktop;
 
@@ -7,11 +8,26 @@ public partial class CreateTaskDesktop : ContentPage
 {
     private readonly ITaskService _taskService;
     private readonly ICategoryService _categoryService;
+    private CreateTaskViewModel _taskViewModel;
     public CreateTaskDesktop(ITaskService taskService, ICategoryService categoryService)
 	{
 		_categoryService = categoryService;
 		_taskService = taskService;
 		InitializeComponent();
-		this.BindingContext = new CreateTaskViewModel(_categoryService,_taskService);
+        _taskViewModel = new CreateTaskViewModel(_categoryService, _taskService);
+        this.BindingContext = _taskViewModel;
 	}
+
+    private void endPicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        startPicker.Date = endPicker.Date;
+    }
+
+    private void startPicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        if(startPicker.Date > endPicker.Date)
+        {
+            startPicker.Date = endPicker.Date;
+        }
+    }
 }
